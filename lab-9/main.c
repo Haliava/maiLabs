d9882c2874aeef526e1c8ca435ef8ed26aa03299
Ivan <grenka388@gmail.com>
2022-11-14 11:25:20 +0300

    Update main.c

--- lab-9/main.c
+++ lab-9/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 const int SMALL_R = 5;
 const int BIG_R = 10;
@@ -18,8 +19,8 @@ int max(int x, int y) {
 }
 
 int isInArea(int i, int j) {
-    int isInSmallCircle = (i - CENTRE_X) + (j - CENTRE_Y) <= SMALL_R * SMALL_R ? 1 : -1;
-    int isInBigCircle = (i - CENTRE_X) + (j - CENTRE_Y) <= BIG_R * BIG_R ? 1 : -1;
+    int isInSmallCircle = (i - CENTRE_X) * (i - CENTRE_X) + (j - CENTRE_Y) * (j - CENTRE_Y) <= SMALL_R * SMALL_R ? 1 : -1;
+    int isInBigCircle = (i - CENTRE_X) * (i - CENTRE_X) + (j - CENTRE_Y) * (j - CENTRE_Y) <= BIG_R * BIG_R ? 1 : -1;
 
     return isInSmallCircle == -1 && isInBigCircle == 1 ? 1 : -1;
 }
@@ -48,8 +49,43 @@ int checkTrajectory(int iterMax, int i, int j, int l) {
     return -1;
 }
 
+void test_sign() {
+    assert(sign(5) == 1);
+    assert(sign(0) == 0);
+    assert(sign(-6) == -1);
+}
+
+void test_abs() {
+    assert(abs(12) == 12);
+    assert(abs(-1231) == 1231);
+    assert(abs(0) == 0);
+}
+
+void test_max() {
+    assert(max(5, 1) == 5);
+    assert(max(1, 5) == 5);
+    assert(max(7, 7) == 7);
+    assert(max(-1, 4) == 4);
+    assert(max(-1, -6) == -1);
+    assert(max(-5, -5) == -5);
+    assert(max(0, 0) == 0);
+}
+
+void test_isInArea() {
+    assert(isInArea(10, 10) == -1);
+    assert(isInArea(15, 15) == 1);
+    assert(isInArea(0, 10) == 1);
+    assert(isInArea(10, 15) == -1);
+    assert(isInArea(200, 10) == -1);
+    assert(isInArea(-6, -8) == -1);
+}
+
 int main() {
-    const int iterMax = 50;
+    const int iterMax = 3;
+    test_abs();
+    test_max();
+    test_sign();
+    test_isInArea();
 
     checkTrajectory(iterMax, 0, -3, -7);
 
