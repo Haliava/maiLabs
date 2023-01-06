ce7dc21e1460e041f641102a415e11989b086252
Ivan <grenka388@gmail.com>
2023-01-06 19:16:12 +0300

    completed kp-4

--- kp-4/main.c
+++ kp-4/main.c
@@ -12,20 +12,20 @@ long double funcVar24(long double x) {
 long double funcVar25(long double x) {
     // sqrt(1-0.4x^2) - arcsin(x) = 0
     // x = arcsin(x) / sqrt(1/(x*x) - 0.4)
-    //return sqrtl(1 - 0.4 * x * x) - asinhl(x);
+    // return sqrtl(1 - 0.4 * x * x) - asinhl(x);
     return asinhl(x) / sqrtl(1 / (x * x) - 0.4);
 }
 
 long double firstDerivative25(long double x) {
-    long double a = (1 / (x * x * x * sinhl(x))) + ((1 / (x * x) - 0.4) / sqrtl(1 - x * x));
-    long double subthingy = -0.4 + 1 / (x * x);
-    long double b = sqrtl(subthingy * subthingy * subthingy);
+    long double a = asinhl(x) / (x * x * x) + (1 / x * x - 0.4) / sqrtl(1 - x * x);
+    long double sub = (1 / x * x - 0.4);
+    long double b = sub * sqrtl(sub);
     return a / b;
 }
 
 // Для funcVar24
 long double binarySearch(double_t left, double_t right) {
-    while ((right - left) > LDBL_EPSILON) {
+    while (fabsl(right - left) > LDBL_EPSILON) {
         left = funcVar24(left) * funcVar24((left + right) / 2) > 0 ? (left + right) / 2 : left;
         right = funcVar24(right) * funcVar24((left + right) / 2) > 0 ? (left + right) / 2 : right;
     }
@@ -44,25 +44,27 @@ long double iterativeSearch(double_t left, double_t right) {
         x = right;
     else {
         x = (left + right) / 2;
-        printf("ERROR");
+        printf("ERROR. Func for #25 Does not converge.\n");
+        return x;
     }
 
-    newX = funcVar25(x);
-    while (fabsl(newX - x) > LDBL_EPSILON)
-        x = funcVar25(x);
+    newX = firstDerivative25(x);
+    while (fabsl(newX - x) > LDBL_EPSILON) {
+        x = newX;
+        newX = firstDerivative25(x);
+    }
 
     return x;
 }
 
 int main() {
-    // TODO нарисовать графики этих фиговин в питоне
     long double x24 = 1.8756;
     long double x25 = 0.7672;
 
     long double ans24 = binarySearch(1, 2);
     long double ans25 = iterativeSearch(0, 1);
 
-    printf("%Lf %Lf", ans24, ans25);
+    printf("#24 - x0: %Lf, x: %Lf \n#25 - x0: %Lf, x: %Lf", x24, ans24, x25, ans25);
 
     return 0;
 }
